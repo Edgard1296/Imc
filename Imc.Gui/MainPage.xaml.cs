f0e4b591ce4d242c7ff628556503a72f3d6cc499
Edgard Joaquin Valadez Aguilar <23470136@campeche.tecnm.mx>
2025-10-02 00:18:24 -0600

    Imc es una aplicacion de .NET MAUI que permite calcular el indice de masa corporal

---
+++ Imc.Gui/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Imc.Gui
+{
+    public partial class MainPage : ContentPage
+    {
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+       
+        }
+    }
+
---
+++ README.md
@@ -0,0 +1,1 @@
+Imc es una aplicacion .NET MAUI que permite calcular el indice de masa corporal y determinar el estado nutricional de una persona
